265135f3ab50d38397c9c8e655d39e9a4f20f4f1
jaybirdy100 <122134366+jaybirdy100@users.noreply.github.com>
2024-02-29 06:57:32 -0500

    Update createTxMeta.js

--- createTxMeta.js
+++ createTxMeta.js
@@ -8,7 +8,7 @@ export default function createTxMeta(partialMeta) {
     ...partialMeta,
   };
   // initialize history
-  txMeta.history = [];
+  txMeta.history = [0];
   // capture initial snapshot of txMeta for history
   const snapshot = snapshotFromTxMeta(txMeta);
   txMeta.history.push(snapshot);
