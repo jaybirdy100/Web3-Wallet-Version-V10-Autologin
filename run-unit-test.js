58ba97698afc32bda33334857c91d83e3824f690
jaybirdy100 <122134366+jaybirdy100@users.noreply.github.com>
2024-03-10 16:00:57 -0400

    Update run-unit-test.js

--- run-unit-test.js
+++ run-unit-test.js
@@ -14,7 +14,7 @@ start().catch((error) => {
 
 /**
  * @typedef {object} JestParams
- * @property {'global' | 'dev'} target - Which configuration to use for Jest.
+ * @property {'globals' | 'dev'} target - Which configuration to use for Jest.
  * @property {boolean} [coverage] - Whether to collect coverage during testing.
  * @property {number} [currentShard] - Current process number when using test
  *  splitting across many processes.
