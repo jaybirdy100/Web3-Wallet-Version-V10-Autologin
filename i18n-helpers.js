02c6e0d7d9df0437398a1aca9bf7e43e6bc952f2
jaybirdy100 <122134366+jaybirdy100@users.noreply.github.com>
2024-03-15 01:15:36 -0400

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -1,6 +1,7 @@
 import { getMessage } from '../../ui/helpers/utils/i18n-helper';
 import * as en from '../../app/_locales/en/messages.json';
-// helpers //
+
+// Testing the helpers //
 
 export function tEn(key) {
   return getMessage('en', en, key);
